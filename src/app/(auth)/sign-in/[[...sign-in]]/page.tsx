3816fbd50746b762b531e1be488e70bbbcccf34a
AHEnrikes <antonio.henrikes@tuni.fi>
2024-05-26 20:00:39 +0300

    feat: Add generateStaticParams function for dynamic routing in sign-in and sign-up pages

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,7 +1,6 @@
 import React from "react"
 import { SignIn } from "@clerk/nextjs"
 
-// Define generateStaticParams if you're using dynamic routing and SSG
 export async function generateStaticParams() {
   return [
     { signIn: [] }
