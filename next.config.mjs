ce8bffb3885490119197c2595361fb5ccfe5313d
AHEnrikes <antonio.henrikes@tuni.fi>
2024-05-28 13:53:02 +0300

    reference error 'ä' fixed

--- next.config.mjs
+++ next.config.mjs
@@ -9,8 +9,6 @@ const nextConfig = {
       },
     ],
   },
-
-  ä
 };
 
 export default nextConfig;
--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,13 +1,6 @@
 import React from "react";
 import "./login.css";
 
-export async function generateStaticParams() {
-  return [
-    { signIn: [] },
-    { signUp: [] }
-  ];
-}
-
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <>
